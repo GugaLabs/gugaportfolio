50480b8c046500655c48ca579f2b210fdb41da37
Ascênio Sanderson <ascenio@hotmail.com>
2025-01-25 18:18:27 -0300

    chore: remove unnecessary # from URL

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:guga_portfolio/firebase_options.dart';
 import 'package:guga_portfolio/pages/cubits/cubit/home_tabs_cubit.dart';
 import 'package:guga_portfolio/pages/home_page.dart';
 import 'package:guga_portfolio/pages/projects_page.dart';
 
 void main() async {
+  usePathUrlStrategy();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
